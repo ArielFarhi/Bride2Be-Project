67b23e8bc986f6f842ae125d664ef3dc8d29c3d5
Arielle Farhi <ariel.farhi@stu.shenkar.ac.il>
2024-12-29 16:46:04 +0200

    change users controllers

--- server/controllers/usersController.js
+++ server/controllers/usersController.js
@@ -1,20 +1,21 @@
 const User = require("../models/User");
 
-exports.usersController = {
-    async getUsers(req, res) {
-        const users = await User.find({});
-        res.json(users);
-    },
-    async registerUser(req, res) {
-        const { name, email, password, role, wedding_date } = req.body;
+async function getUsers(req, res) {
+    const users = await User.find({});
+    res.json(users);
+}
 
-        try {
-            const newUser = new User({ name, email, password, role, wedding_date });
-            await newUser.save();
-            res.status(201).json({ message: "User registered successfully!" });
-        } catch (error) {
-            console.error("Error registering user:", error);
-            res.status(500).json({ error: "Failed to register user" });
-        }
+async function registerUser(req, res) {
+    const { name, email, password, role, wedding_date } = req.body;
+
+    try {
+        const newUser = new User({ name, email, password, role, wedding_date });
+        await newUser.save();
+        res.status(201).json({ message: "User registered successfully!" });
+    } catch (error) {
+        console.error("Error registering user:", error);
+        res.status(500).json({ error: "Failed to register user" });
     }
-};
+}
+
+exports.usersController = { getUsers, registerUser };
