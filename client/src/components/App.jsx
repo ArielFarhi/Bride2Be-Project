d7f114773577b6219b499b81e5fc6c413307baae
omerguetta <omer.guetta@stu.shenkar.ac.il>
2024-12-30 12:31:50 +0200

    connect between login page to register page

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,14 +1,34 @@
-import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
-import Header from './Header';
-import Signup from './Signup';
+// import React from 'react';
+// import { BrowserRouter as Router } from 'react-router-dom';
+// import Header from './Header';
+// import Signup from './Signup';
+// import Signin from './Signin';
+
+// function App() {
+//   return (
+//     <Router>
+//       <div className="App">
+//         {/* <Signup /> */}
+//         <Signin />
+//       </div>
+//     </Router>
+//   );
+// }
+
+// export default App;
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Signup from "./Signup";
+import Signin from "./Signin";
 
 function App() {
   return (
     <Router>
       <div className="App">
-        <Header />
-        <Signup />
+        <Routes>
+          <Route path="/" element={<Signin />} />
+          <Route path="/register" element={<Signup />} />
+        </Routes>
       </div>
     </Router>
   );
