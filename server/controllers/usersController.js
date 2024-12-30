9ef993fa1472c0355aa5f3a39991534ea6f812dc
omerguetta <omer.guetta@stu.shenkar.ac.il>
2024-12-30 14:13:29 +0200

    connect login to server

--- server/controllers/usersController.js
+++ server/controllers/usersController.js
@@ -1,17 +1,45 @@
+const bcrypt = require("bcrypt");
 const User = require("../models/User");
+
+const loginUser = async (req, res) => {
+    try {
+        const { username, password } = req.body;
+
+        const user = await User.findOne({ username });
+        if (!user) {
+            return res.status(404).json({ error: "User not found" });
+        }
+
+        const isMatch = await bcrypt.compare(password, user.password);
+        if (!isMatch) {
+            return res.status(401).json({ error: "Invalid username or password" });
+        }
+
+        res.status(200).json({
+            message: "Login successful",
+            user: {
+                userID: user._id,
+                username: user.username,
+                role: user.role,
+                coupleType: user.coupleType,
+            },
+        });
+    } catch (error) {
+        console.error("Error logging in user:", error);
+        res.status(500).json({ error: "Failed to login user" });
+    }
+};
 
 const registerUser = async (req, res) => {
     try {
         console.log(req.body);
         const { fullName, username, email, password, role, coupleType, wedding_date } = req.body;
 
-        // Check if user already exists
         const existingUser = await User.findOne({ $or: [{ email }, { username }] });
         if (existingUser) {
             return res.status(400).json({ error: "Email or username is already in use" });
         }
 
-        // Create new user
         const newUser = new User({
             fullName,
             username,
@@ -32,4 +60,5 @@ const registerUser = async (req, res) => {
 
 module.exports = {
     registerUser,
+    loginUser,
 };
--- server/routers/usersRouter.js
+++ server/routers/usersRouter.js
@@ -1,8 +1,9 @@
-const { Router } = require('express');
-const { registerUser } = require('../controllers/usersController');
+const { Router } = require("express");
+const { registerUser, loginUser } = require("../controllers/usersController");
 
 const usersRouter = new Router();
 
-usersRouter.post('/register', registerUser);
+usersRouter.post("/register", registerUser);
+usersRouter.post("/login", loginUser);
 
 module.exports = { usersRouter };
