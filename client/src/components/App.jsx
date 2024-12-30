cf9cbc7b45eb3da33468a595dde082a1ead03aaa
omerguetta <omer.guetta@stu.shenkar.ac.il>
2024-12-30 20:14:15 +0200

    delete unnecessary code from App.jsx

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,21 +1,3 @@
-// import React from 'react';
-// import { BrowserRouter as Router } from 'react-router-dom';
-// import Header from './Header';
-// import Signup from './Signup';
-// import Signin from './Signin';
-
-// function App() {
-//   return (
-//     <Router>
-//       <div className="App">
-//         {/* <Signup /> */}
-//         <Signin />
-//       </div>
-//     </Router>
-//   );
-// }
-
-// export default App;
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Signup from "./Signup";
