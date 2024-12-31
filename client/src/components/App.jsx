5832ecd5528293a6bcfd87c2a47bfea0c4bf1395
omerguetta <omer.guetta@stu.shenkar.ac.il>
2024-12-31 15:41:21 +0200

    connection home page

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Signup from "./Signup";
 import Signin from "./Signin";
+import HomePage from "./HomePage";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Signin />} />
           <Route path="/register" element={<Signup />} />
+          <Route path="/home" element={<HomePage />} />
         </Routes>
       </div>
     </Router>
@@ -17,3 +19,4 @@ function App() {
 }
 
 export default App;
+
