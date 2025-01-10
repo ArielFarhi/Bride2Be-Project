925fab248d6701be80c8ff9b4c1364789efd66e3
omerguetta <omer.guetta@stu.shenkar.ac.il>
2025-01-10 18:15:46 +0200

    add to userProfile.jsx

---
+++ client/src/components/UserProfile.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+function UserProfile({ user }) {
+    if (!user) {
+        return <p>Please log in to view your profile.</p>;
+    }
+
+    return (
+        <div className="profile-container">
+            <div className="profile-header">
+                <img className="profile-picture"/>
+                <h1>{`${user.partnerOneName} & ${user.partnerTwoName}`}</h1>
+                <h2>{user.username}</h2>
+            </div>
+            <div className="profile-details">
+                <p><strong>Email:</strong> {user.email}</p>
+                <p><strong>Phone:</strong> {user.phone}</p>
+                <p><strong>Wedding Date:</strong> {new Date(user.wedding_date).toLocaleDateString()}</p>
+                <p><strong>Couple Type:</strong> {user.coupleType}</p>
+                <p><strong>Role:</strong> {user.role}</p>
+            </div>
+            <button className="edit-profile-button">Edit Profile</button>
+        </div>
+    );
+}
+
+
+export default UserProfile;
